107506e734ebdc34994c6122292b7a3838305c0f
nikimat36 <74063319+nikimat36@users.noreply.github.com>
2021-01-08 10:13:21 +0100

    Update script.js
    Day/Night-Light update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var hod = document.getElementById("hr");
 var min = document.getElementById("mn");
 var sec = document.querySelector(".sc");
+ 
 setInterval(() =>{
     var d = new Date();
     var h = d.getHours();
@@ -9,4 +10,10 @@ setInterval(() =>{
     hod.style.setProperty('--x', (h + (m/60))*30 + "deg");
     min.style.setProperty('--y', m*6 + "deg");
     sec.style.setProperty('--z', s*6 + "deg");
+
+    // Day/Night-Light
+    var a = (Math.abs((h + (m/60)) - 12)/12.2) -0.1;
+    var cover = document.querySelector(".dayNight");
+    cover.style.setProperty('--b', a);
+
 });
